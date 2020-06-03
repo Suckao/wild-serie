bc943210a646d740b31462352bb7703441987910
Suckao <johnny.victor.leger@gmail.com>
2020-06-03 17:28:43 +0200

    Symfony quest 14

--- src/Entity/Program.php
+++ src/Entity/Program.php
@@ -61,9 +61,20 @@ class Program
      */
     private $number;
 
+    /**
+     * @ORM\ManyToMany(targetEntity=Actor::class, mappedBy="programs")
+     */
+    private $actors;
+
     public function __construct()
     {
         $this->number = new ArrayCollection();
+        $this->actors = new ArrayCollection();
+    }
+
+    public function __toString()
+    {
+        return $this->title;
     }
 
     public function getId(): ?int
@@ -169,6 +180,34 @@ class Program
             if ($number->getProgramId() === $this) {
                 $number->setProgramId(null);
             }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Actor[]
+     */
+    public function getActors(): Collection
+    {
+        return $this->actors;
+    }
+
+    public function addActor(Actor $actor): self
+    {
+        if (!$this->actors->contains($actor)) {
+            $this->actors[] = $actor;
+            $actor->addProgram($this);
+        }
+
+        return $this;
+    }
+
+    public function removeActor(Actor $actor): self
+    {
+        if ($this->actors->contains($actor)) {
+            $this->actors->removeElement($actor);
+            $actor->removeProgram($this);
         }
 
         return $this;
